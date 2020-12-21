2584644db2b006f8dd30b243ebcfcbcf6cdea4fc
Nico Prananta <nicnocquee@users.noreply.github.com>
2020-12-21 12:10:42 +0100

    Github workflow (#7)
    * add github workflow to lint the shell script
    * fix the shellscript based on shellcheck warnings
    Co-authored-by: Nico Prananta <nico@hyperjump.tech>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Starting action"
 
-eval $(ssh-agent -s)
+eval "$(ssh-agent -s)"
 
 echo "Setting up SSH"
 mkdir -p ~/.ssh
@@ -11,26 +11,23 @@ echo -e "$DOKKU_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan "$HOST" >> ~/.ssh/known_hosts
 
-cd "$GITHUB_WORKSPACE"
+cd "$GITHUB_WORKSPACE" || exit
 
 DEFAULT_BRANCH=${DEFAULT_BRANCH:-'master'}
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-
-if [[ $CURRENT_BRANCH == $DEFAULT_BRANCH && -n $DOMAIN ]]; then
-  APP_NAME=$DOMAIN
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ $CURRENT_BRANCH == "$DEFAULT_BRANCH" && -n $DOMAIN ]]; then
+    APP_NAME=$DOMAIN
 else
-  APP_NAME=${CURRENT_BRANCH/\//-}
+    APP_NAME=${CURRENT_BRANCH/\//-}
 fi
 
 echo "Checking if app exists"
-ssh "dokku@$HOST" apps:exists $APP_NAME
-
-if [[ $? != 0 ]]; then
-  echo "The app does not exist yet, creating the app"
-  ssh "dokku@$HOST" apps:create $APP_NAME
+if ! ssh "dokku@$HOST" 'apps:exists $APP_NAME'; then
+    echo "The app does not exist yet, creating the app"
+    ssh "dokku@$HOST" 'apps:create $APP_NAME'
 fi
 
 echo "Deploying to host"
 git fetch --unshallow
-git remote add $APP_NAME "dokku@$HOST:$APP_NAME"
-git push -f $APP_NAME "$CURRENT_BRANCH:master"
+git remote add "$APP_NAME" "dokku@$HOST:$APP_NAME"
+git push -f "$APP_NAME" "$CURRENT_BRANCH:master"
